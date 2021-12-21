e6f048ee2554106c0eccb40710aa1e4b527d04e9
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-21 11:02:00 +0530

    learning javadouble.java

---
+++ javadouble.java
@@ -0,0 +1,35 @@
+class javadouble
+
+{
+ public static void main (String[]args)
+{
+
+double value []={66.001d,644.64646d,54.66464d,5245.6464d,122.546d,5484.54d,12.545464d,54545.1654646d,164.6116d,55.54646d};
+
+System.out.println(value[0]);
+
+
+System.out.println(value[1]);
+
+
+System.out.println(value[2]);
+
+System.out.println(value[3]);
+
+System.out.println(value[4]);
+
+System.out.println(value[5]);
+
+System.out.println(value[6]);
+
+
+System.out.println(value[7]);
+
+
+System.out.println(value[8]);
+
+
+System.out.println(value[9]);
+
+}
+}
